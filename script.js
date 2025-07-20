489feddcfd41cc068c95a7dff92544047bf1f037
Luigi Henry C. Araujo <87998061+luigihenrydev@users.noreply.github.com>
2025-07-20 12:12:31 -0300

    Adicionando mensagem de Boas Vindas ao acessar a Página

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+window.addEventListener('load', () => {
+  const modal = document.getElementById('welcomeModal');
+  const closeBtn = document.getElementById('closeModal');
+
+  modal.classList.add('show');
+
+  closeBtn.addEventListener('click', () => {
+    modal.classList.remove('show');
+  });
+});
+
 const toggleButton = document.getElementById('darkModeToggle');
 
 toggleButton.addEventListener('click', () => {
